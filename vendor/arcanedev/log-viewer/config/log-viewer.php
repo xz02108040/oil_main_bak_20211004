dd9195d542cc153decc5d0a59ce19f3b1f301615
xz02108040 <xz02108040@gmail.com>
2021-10-06 14:22:33 +0800

    項次372，Log Viewer，修正主題與語言

--- vendor/arcanedev/log-viewer/config/log-viewer.php
+++ vendor/arcanedev/log-viewer/config/log-viewer.php
@@ -30,7 +30,7 @@ return [
      |    'pl', 'pt-BR', 'ro', 'ru', 'sv', 'th', 'tr', 'zh-TW', 'zh'
      */
 
-    'locale'        => 'auto',
+    'locale'        => 'zh-TW',
 
     /* -----------------------------------------------------------------
      |  Theme
@@ -40,7 +40,7 @@ return [
      |  Make your own theme by adding a folder to the views directory and specifying it here.
      */
 
-    'theme'         => 'bootstrap-4',
+    'theme'         => 'bootstrap-3',
 
     /* -----------------------------------------------------------------
      |  Route settings
